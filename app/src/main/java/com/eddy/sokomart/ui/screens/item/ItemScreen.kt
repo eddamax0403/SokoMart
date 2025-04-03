905c5daf2c19a578ffeebccd497eda712c9ebf9a
eddamax0403 <mwanziaedwin5@gmail.com>
2025-04-03 10:54:06 +0300

    Second commit

--- app/src/main/java/com/eddy/sokomart/ui/screens/item/ItemScreen.kt
+++ app/src/main/java/com/eddy/sokomart/ui/screens/item/ItemScreen.kt
@@ -1,21 +1,47 @@
 package com.eddy.sokomart.ui.screens.item
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.eddy.sokomart.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -34,17 +60,163 @@ fun ItemScreen(navController: NavController){
             colors = TopAppBarDefaults.topAppBarColors(
                 containerColor = Color.DarkGray,
                 titleContentColor = Color.White,
-                navigationIconContentColor = Color.White
+                navigationIconContentColor = Color.White,
+                actionIconContentColor = Color.Yellow
+
             ),
             navigationIcon = {
                 IconButton(onClick = {}) {
                     Icon(imageVector = Icons.Default.Menu, contentDescription = "")
+                }
+            },
+            actions = {
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Default.ShoppingCart, contentDescription = "")
+                }
 
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Default.Notifications, contentDescription = "")
+                }
+
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Default.Search, contentDescription = "")
+                }
+
+            }
+        )
+        //end of TopAppbar
+
+        Image(
+            painter = painterResource(R.drawable.item),
+            contentDescription = "Item",
+            modifier = Modifier.fillMaxWidth().height(540.dp),
+            contentScale = ContentScale.FillWidth
+        )
+
+        //Searchbar
+        Spacer(modifier = Modifier.height(10.dp))
+
+
+        var search by remember { mutableStateOf("") }
+        OutlinedTextField(
+            value = search,
+            onValueChange = {search = it},
+            modifier = Modifier.fillMaxWidth().padding(start = 10.dp, end = 10.dp),
+            leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "")},
+            placeholder = { Text(text = "Search...") }
+        )
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        //Row
+        Row (modifier = Modifier.padding(start = 10.dp)){
+            Image(
+                painter = painterResource(R.drawable.home),
+                contentDescription = "Home",
+                modifier = Modifier.width(200.dp).height(150.dp).clip(shape = RoundedCornerShape(10.dp)),
+                contentScale = ContentScale.FillWidth
+            )
+
+            //Horizontal spacer
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Column (){
+                Text(
+                    text = "Men's Leather Jacket",
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 20.sp
+                )
+
+                Text(
+                    text = "Casual Wear",
+                    fontSize = 15.sp
+                )
+
+                Text(
+                    text = "Before:Ksh.1800",
+                    fontSize = 15.sp,
+                    textDecoration = TextDecoration.LineThrough
+                )
+
+                Text(
+                    text = "Now:Ksh.1500",
+                    fontSize = 15.sp
+                )
+
+                Row {
+                    Icon(imageVector = Icons.Default.Star, contentDescription = "", tint = Color.Green)
+                    Icon(imageVector = Icons.Default.Star, contentDescription = "", tint = Color.Green)
+                    Icon(imageVector = Icons.Default.Star, contentDescription = "", tint = Color.Green)
+                    Icon(imageVector = Icons.Default.Star, contentDescription = "", tint = Color.Green)
 
                 }
             }
 
-        )
+        }
+    //end of row
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        //Row
+        Row (modifier = Modifier.padding(start = 10.dp)){
+            Image(
+                painter = painterResource(R.drawable.home),
+                contentDescription = "Home",
+                modifier = Modifier.width(200.dp).height(150.dp).clip(shape = RoundedCornerShape(10.dp)),
+                contentScale = ContentScale.FillWidth
+            )
+
+            //Horizontal spacer
+            Spacer(modifier = Modifier.width(10.dp))
+
+            Column (){
+                Text(
+                    text = "Men's Leather Jacket",
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 20.sp
+                )
+
+                Text(
+                    text = "Casual Wear",
+                    fontSize = 15.sp
+                )
+
+                Text(
+                    text = "Before:Ksh.1800",
+                    fontSize = 15.sp,
+                    textDecoration = TextDecoration.LineThrough
+                )
+
+                Text(
+                    text = "Now:Ksh.1500",
+                    fontSize = 15.sp
+                )
+
+                Row {
+                    Icon(imageVector = Icons.Default.Star, contentDescription = "", tint = Color.Green)
+                    Icon(imageVector = Icons.Default.Star, contentDescription = "", tint = Color.Green)
+                    Icon(imageVector = Icons.Default.Star, contentDescription = "", tint = Color.Green)
+                    Icon(imageVector = Icons.Default.Star, contentDescription = "", tint = Color.Green)
+
+                }
+
+
+
+            }
+
+
+
+
+
+        }
+
+
+
+
+        //end of row
+
+
+
 
 
 
